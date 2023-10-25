3185dba3137c2cc80df784691c64985137b005e0
Shakib Absar <sabsar42@gmail.com>
2023-10-25 18:01:45 +0600

    Medical History - Card View Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'LogInScreen.dart';
+import 'medical_history/Health_Record_Screen.dart';
+import 'signUp_logIn/LogInScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor:Color.fromRGBO(3, 74, 166, 1.0)),
         useMaterial3: true,
       ),
-      home: const LogInScreen(),
+      home:  HealthRecordScreen(),
     );
   }
 }
