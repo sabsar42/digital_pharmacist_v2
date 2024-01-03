d06146624346ccb01bd10d54dfda419d89bb725c
shuvosonjoy <shuvosdg123@gmail.com>
2024-01-03 22:42:54 +0600

    onboard_images_from asset

--- lib/OnBoard/OnBoard.dart
+++ lib/OnBoard/OnBoard.dart
@@ -8,7 +8,8 @@ class OnBoard extends StatelessWidget {
   List<PageViewModel>getPages(){
     return [
       PageViewModel(
-        image: Image.network('https://cdn-icons-png.flaticon.com/512/7310/7310705.png'),  //
+       // image: Image.network('https://cdn-icons-png.flaticon.com/512/7310/7310705.png'),
+        image:Image.asset('assets/images/onboard_1.png'),
         title: "Get medical Consultation",
         body: "Commmunicate with the finest doctors in your area",
           decoration: PageDecoration(
@@ -17,13 +18,15 @@ class OnBoard extends StatelessWidget {
 
       ),
       PageViewModel(
-        image: Image.network('https://cdn-icons-png.flaticon.com/512/1546/1546140.png'),
-        title: "Buy medicines Online",
-        body: "Most conveneient way to get medicine at your doorstep",
+        // image: Image.network('https://cdn-icons-png.flaticon.com/512/1546/1546140.png'),
+        image:Image.asset('assets/images/onboard_3.png'),
+        title: "Medication Reminders",
+        body: "Stay on top of your health effortlessly with our Medication Reminders",
 
       ),
       PageViewModel(
-        image: Image.network('https://cdn-icons-png.flaticon.com/512/4003/4003759.png'),
+       // image: Image.network('https://cdn-icons-png.flaticon.com/512/4003/4003759.png'),
+        image:Image.asset('assets/images/onboard_2.png'),
         title: "Keep EHR files",
         body: "Store all of your EHR files in one place",
 
