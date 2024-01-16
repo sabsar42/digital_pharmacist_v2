71a9f14003e3b376022b8a7725e3e05bb6aa0503
Shakib Absar <sabsar42@gmail.com>
2024-01-16 18:09:05 +0600

    Gemini Generates Prescriptions Summary

--- lib/Camera Scanner/gemini_text_to_medRecord.dart
+++ lib/Camera Scanner/gemini_text_to_medRecord.dart
@@ -8,21 +8,22 @@ import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
 
-import '../Gemini Digi-BOT/widgets/chat_input_box.dart';
 import '../Gemini Digi-BOT/widgets/item_image_view.dart';
+import '../style.dart';
 
 class TextRecogSectionTextStreamInput extends StatefulWidget {
   final String? initialText;
 
-  const TextRecogSectionTextStreamInput({Key? key, required this.initialText}) : super(key: key);
+  const TextRecogSectionTextStreamInput({Key? key, required this.initialText})
+      : super(key: key);
 
   @override
-  State<TextRecogSectionTextStreamInput> createState() => _SectionTextInputStreamState();
+  State<TextRecogSectionTextStreamInput> createState() =>
+      _SectionTextInputStreamState();
 }
 
-class _SectionTextInputStreamState extends State<TextRecogSectionTextStreamInput> {
-  final ImagePicker picker = ImagePicker();
-  final controller = TextEditingController();
+class _SectionTextInputStreamState
+    extends State<TextRecogSectionTextStreamInput> {
   final gemini = Gemini.instance;
   String? searchedText, _finishReason;
   List<Uint8List>? images;
@@ -45,7 +46,6 @@ class _SectionTextInputStreamState extends State<TextRecogSectionTextStreamInput
 
   void promptGemini(String text) {
     searchedText = text;
-    controller.clear();
     gemini.streamGenerateContent(searchedText!, images: images).listen((value) {
       setState(() {
         images = null;
@@ -61,91 +61,95 @@ class _SectionTextInputStreamState extends State<TextRecogSectionTextStreamInput
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (searchedText != null)
-          MaterialButton(
-            color: Colors.blue.shade700,
-            onPressed: () {
-              setState(() {
-                searchedText = null;
-                finishReason = null;
-              });
-            },
-            child: Text('search: $searchedText'),
-          ),
-        Expanded(
-          child: GeminiResponseTypeView(
-            builder: (context, child, response, loading) {
-              if (loading) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 90),
-                  child: SizedBox(
-                    width: 80.0,
-                    height: 80.0,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: SvgPicture.asset(
-                        'assets/images/digi-pharma-prussian.svg',
-                      ),
-                    ),
-                  ),
-                );
-              }
-
-              if (response != null) {
-                return Markdown(
-                  data: response,
-                  selectable: true,
-                );
-              } else {
-                return const Center(child: Text('Search something!'));
-              }
-            },
-          ),
-        ),
-
-        if (finishReason != null) Text(finishReason!),
-
-        if (images != null)
-          Container(
-            height: 120,
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            alignment: Alignment.centerLeft,
-            child: Card(
-              child: ListView.builder(
-                itemBuilder: (context, index) => ItemImageView(
-                  bytes: images!.elementAt(index),
-                ),
-                itemCount: images!.length,
-                scrollDirection: Axis.horizontal,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: AssetImage(
+                "assets/images/dashboard_card.png",
               ),
+              fit: BoxFit.cover,
             ),
           ),
+          child: AppBar(
+            toolbarHeight: 45,
+            leading: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.purple.shade50,
+                size: 25,
+              ),
+            ),
+            title: Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Text(
+                "AI Generated Prescription ",
+                style: siz22White(),
+              ),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GeminiResponseTypeView(
+              builder: (context, child, response, loading) {
+                if (loading) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: SizedBox(
+                      width: 80.0,
+                      height: 80.0,
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: SvgPicture.asset(
+                          'assets/images/digi-pharma-prussian.svg',
+                        ),
+                      ),
+                    ),
+                  );
+                }
 
-        ChatInputBox(
-          controller: controller,
-          onClickCamera: () {
-            picker.pickMultiImage().then((value) async {
-              final imagesBytes = <Uint8List>[];
-              for (final file in value) {
-                imagesBytes.add(await file.readAsBytes());
-              }
-
-              if (imagesBytes.isNotEmpty) {
-                setState(() {
-                  images = imagesBytes;
-                });
-              }
-            });
-          },
-          onSend: () {
-            if (controller.text.isNotEmpty) {
-              promptGemini(controller.text);
-            }
-          },
-        )
-      ],
+                if (response != null) {
+                  return Markdown(
+                    data: response,
+                    selectable: true,
+                  );
+                } else {
+                  return const Center(child: Text('Search something!'));
+                }
+              },
+            ),
+          ),
+          if (finishReason != null) Text(finishReason!),
+          if (images != null)
+            Container(
+              height: 120,
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              alignment: Alignment.centerLeft,
+              child: Card(
+                child: ListView.builder(
+                  itemBuilder: (context, index) => ItemImageView(
+                    bytes: images!.elementAt(index),
+                  ),
+                  itemCount: images!.length,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
